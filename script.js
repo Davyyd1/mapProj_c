6b18dcdf2812016556bc9a7ac6f11b1ab7761499
Davyyd1 <d.dav889@gmail.com>
2023-11-20 14:20:30 +0200

    geolocation API displaying map

--- script.js
+++ script.js
@@ -11,18 +11,28 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position) {
-        const {latitude} = position.coords; 
-        const {longitude} = position.coords;
-        console.log(latitude,longitude);
-        
-        
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},13z?entry=ttu`
+      );
 
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
 
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
 
-
-    }, function(){
-        alert(`Could not get your position`);
-    })
+      L.marker(coords).addTo(map).bindPopup('hei').openPopup();
+    },
+    function () {
+      alert(`Could not get your position`);
+    }
+  );
 }
