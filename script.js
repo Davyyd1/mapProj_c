8ded9f257c6663f14c70981722b53c395abba6bf
Davyyd1 <d.dav889@gmail.com>
2023-11-21 09:03:29 +0200

    displaying marker after form submit

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+
+let map, mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -21,7 +24,7 @@ if (navigator.geolocation) {
       );
 
       const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -29,26 +32,35 @@ if (navigator.geolocation) {
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(map);
 
-      
-
-      map.on('click', function(mapEvent){
-        console.log(mapEvent);
-        const {lat, lng} = mapEvent.latlng;
-        
-        L.marker([lat,lng]).addTo(map).bindPopup(L.popup({
-          maxWidth:250,
-          minWidth:150,
-          autoClose: false,
-          closeOnClick: false,
-          className: 'running-popup'
-        }))
-        .setPopupContent('Workout')
-        .openPopup();
-      })
-
+      // handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
     function () {
       alert(`Could not get your position`);
     }
   );
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  //display marker
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 150,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
